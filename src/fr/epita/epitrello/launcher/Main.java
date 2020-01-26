0f2fe96caf54c331823dbd3360e51504efa1c975
kuwar <kuwarsaurav21@gmail.com>
2020-01-26 01:02:44 +0100

    added main file

---
+++ src/fr/epita/epitrello/launcher/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.epita.epitrello.launcher;
+
+/**
+ * @author kuwar
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
